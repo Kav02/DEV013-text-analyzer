a413e6e9a9b058412414efdcfdd7ea24cd57f8d3
Korin <kav02@outlook.com>
2023-12-19 20:24:44 -0600

    Tres primeras funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,23 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return text.split(" ").length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length; //Se usa length para obtener el total de caracteres
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    //Remover los espacios
+    let withoutSpaces = text.replace(/[^\w\s]/g,"")//Busca lo que no son letras, ni numeros y los reemplaza
+      .replace(/\s/g, ""); // Busca los espacios en blanco /\s/, la g indica global para que busque todos y "" elimina estos espacios
+    console.log(withoutSpaces)
+    return withoutSpaces.length; //Contar sin espacios
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -4,14 +4,36 @@ import analyzer from './analyzer.js';
 
 // Registrar el texto que el usuario ingresa
 //Variable con el texto del usuario
-let input_text = document.querySelector("textarea[name='user-input']");
-function capture_input(){
-    let value = input_text.value
-}   
 
-input_text.addEventListener('input',capture_input);
+const inputText = document.querySelector("textarea[name='user-input']");
+let texto;
+function capture_input(){
+  texto = inputText.value;
+}
+inputText.addEventListener('input',capture_input);
+
 //Limpiar el valor de la caja de texto
 function clear_textarea(){
-    input_text.value = '';
+  inputText.value = '';
 }
 document.getElementById('reset-button').addEventListener('click',clear_textarea);
+
+//Colocar el resultado
+
+const characterCountLi = document.querySelector(".lista[data-testid='character-count']");
+const characternoCountLi = document.querySelector(".lista[data-testid='character-no-spaces-count']");
+const wordCountLi = document.querySelector(".lista[data-testid='word-count']");
+const numberCountLi = document.querySelector(".lista[data-testid='number-count']");
+const numberSumLi = document.querySelector(".lista[data-testid='number-sum']");
+const wordAverageLi = document.querySelector(".lista[data-testid='word-lenght-average']");
+
+inputText.addEventListener('input',() => {  
+  const totalCharacter = analyzer.getCharacterCount(texto);
+  const totalCharacterNo = analyzer.getCharacterCountExcludingSpaces(texto);
+  const totalWords = analyzer.getWordCount(texto);
+  characterCountLi.textContent = `Caracteres: ${totalCharacter}`;
+  characternoCountLi.textContent = `Caracteres sin espacios: ${totalCharacterNo}`;
+  wordCountLi.textContent = `Palabras: ${totalWords}`;
+
+  console.log(totalCharacter);
+});
