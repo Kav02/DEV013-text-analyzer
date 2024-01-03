5a2464c4bf0442e7c1acaf666955336eb07f22a6
Korin <kav02@outlook.com>
2024-01-03 11:38:49 -0600

    Corregido problema con el número en el promedio

--- src/analyzer.js
+++ src/analyzer.js
@@ -24,17 +24,18 @@ const analyzer = {
     //Remover los espacios
     
     const withoutSpaces = text
-      .replace(/[^\w\s]/g, "") //Busca lo que no son letras, ni numeros y los reemplaza
-      .replace(/\s/g, ""); // Busca los espacios en blanco /\s/, la g indica global para que busque todos y "" elimina estos espacios
+      .replace(/[^\w]/g, "") //Busca lo que no son letras, ni numeros y los reemplaza por un vacío. [^] Sirve para indicar que busque lo opuesto. \w es para alfanumérico y \s espacios en blanco 
+    //.replace(/\s/g, ""); // Busca los espacios en blanco /\s/, la g indica global para que busque todos y "" elimina estos espacios
     return withoutSpaces.length; //Contar sin espacios
   },
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    //Total de caracteres sin espacios entre la cantidad de palabras
+    //Total de caracteres sin espacios dividido entre la cantidad de palabras
     const sumCaracteres = text.replace(/ /g,"").length;
     const sumWords = analyzer.getWordCount(text);
     const averageWords = sumCaracteres / sumWords;
-    return Number(averageWords).toFixed(2);
+    const promedioPalabras = averageWords.toFixed(2);
+    return Number(promedioPalabras);
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
